05bce0c83c2bfc7df406a938125139502b50ae8f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-06 10:29:52 +0100

    change id on class in css for ul

--- js/main.js
+++ js/main.js
@@ -4,11 +4,6 @@ const strip = (bandName) => {
     return bandName.replace(/^(a |the |an )/i, "").trim();
 }
 
-const sortedBands = bands.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else {
-        return -1;
-    }
-}); 
-console.log(sortedBands)
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector(".bands").innerHTML = bands.map(band => `<li>${band}</li>`).join("");
